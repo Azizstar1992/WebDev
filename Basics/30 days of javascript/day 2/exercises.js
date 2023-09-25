815398e366b1f7438424029f010f28dc95f99e3e
Azizstar1992 <126476257+Azizstar1992@users.noreply.github.com>
2023-09-25 17:42:38 +0100

    day 2

--- Basics/30 days of javascript/day 2/exercises.js
+++ Basics/30 days of javascript/day 2/exercises.js
@@ -124,12 +124,23 @@ console.log(ja[l])
 
 console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125')
 
-//'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
 
+
+//                                  exercise 3 
+//'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+let sentence2 = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+console.log(sentence2.match(/love/gi).length)
 //Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
 //Clean the following text and find the most frequent word (hint, use replace and regular expressions).
 const sentence1 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-
+let string3 = sentence1.replace(/[^A-Za-z0-9' '']/gi,'')
+console.log(string3)
+let s = ""
+let count = 0
+for(let i in string3.split(" "))
+{
+    
+}
 //Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 
